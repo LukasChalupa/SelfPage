3216b28fd19162762b429e64aef94591d6a162e2
lukasChalupa <jarjarbinks70@gmail.com>
2016-08-08 18:25:31 +0200

    fix reappear on hovering the same element

--- script.js
+++ script.js
@@ -6,32 +6,39 @@ $(document).ready(function(){
 	  		     "transform"        : "rotate(" + deg + "deg)"});
 	}
 
+	var active = "";
 	$(".menuItems li").hover(function(){
 		var degree;
 		var id = $(this).attr("id");
-		$(".content > div").hide();
+		if(active != $(this).attr("id")) {
+			$(".content > div").hide();
+		}
 		switch(id) {
 			case "first": 
 				degree = -50;
 				$(".aboutMe").show(500);
+				active = "first";
 				break;
 			case "second":
 				degree = 50;
 				$(".education").show(500);
+				active = "second";
 				break;
 			case "third":
 				degree = -20;
 				$(".experience").show(500);
+				active = "third";
 				break;
 			case "fourth":
 				degree = 20;
 				$(".contact").show(500);
+				active = "fourth";
 				break;
 			default: degree = 0;
 		}
 		rotate($(".rotator"), degree);
 		
 	}, function(){
-		rotate($(".rotator"),0);
+		//rotate($(".rotator"),0);
 	});
 });
